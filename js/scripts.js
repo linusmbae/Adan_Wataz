ef29214fecd3648402502778f6d465297efe0bed
candycrushpro <bradleynilmar6702@gmail.com>
2020-03-03 17:20:20 +0300

    changes on vission and mission in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+    $('#view').click(function(){
+        // $('.toggle').hide();
+        $('.toggle').toggle(2000);
+    });
     
+});
+$(document).ready(function(){
+    $('#').click(function(){
+        $('.show').toggle(500);
+    });
 })
