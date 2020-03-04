ff2fb950bfe3c232e23649d7bb7cb2355d8b1c5e
mwangimk <mwangikingstonemk@gmail.com>
2020-03-04 11:53:44 +0300

    toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $("#clickable1").click(function(){
-      $("#domesticShowing").toggle();
       $("#domesticHidden").toggle();
     });
   });
+
+  $(document).ready(function() {
+    $("#clickable2").click(function(){
+        $("#commercialHidden").toggle();
+      });
+    });
+    // function clickable2()
+    // {
+    //   ("#commercialShowing").toggle();
+    //   ("#commercialHidden").toggle();
+    // }
