52f66c9222d515af20e44b4b54dd54df17a55e76
felix <felixxnathann@gmail.com>
2020-03-04 09:12:22 +0300

    new changes

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function myFuntion()
+{
+  var name=document.getElementById("nameDetails").value();
+  var email=document.getElementById("email").value();
+  alert(name+"we have received your message. Thank you for reaching out to us.");
+};
