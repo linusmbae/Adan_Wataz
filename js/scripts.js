25de8b81ebe679158b8e09499e5c6017be96ff65
Linus Mutugi <linusmutugi5178@gmail.com>
2020-03-06 10:52:33 +0300

    complete

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,55 @@ $(document).ready(function() {
     $(".flip").click(function(){
       $("#panel").toggle(500);
     });
-
+    $("#domestic").unbind("click").click(function()
+    {
+        $("#serviceSuppliers").empty();
+        $("#serviceSuppliers").append(
+                                        "<br><br><div class='row' id='appendedDomestic'>"+
+                                            "<div class='col-md-2'>"+
+                                                "<h2>Drinking</h2>"+
+                                                "<img src='images/water-glass.jpg' id='drink'>"+
+                                            "</div>"+
+                                            "<div class='col-md-2'>"+
+                                                "<h2>Laundry</h2>"+
+                                                "<img src='images/laundry.jpg'id='makeLaundry'>"+
+                                            "</div>"+
+                                            "<input class='form-control-sm' type='number' id='totalLiters' value='1'>"+
+                                        "</div>"+
+                                        "<button class='btn-primary' id='myButton'>Submit</button>"
+                                     );
+    $("#drink").unbind("click").click(function()
+    {
+        $("#show").empty();
+        $("#show").append(
+                                        "<div>"+
+                                        "<p id='displayLitterm'>1 Litre @ Ksh.</p>"+
+                                        "<p id='displayLitter'>50</p>"+
+                                        "</div>"
+                                    );
+    });
+    $("#makeLaundry").unbind("click").click(function()
+    {
+        $("#show").empty();
+        $("#show").append(
+                                        "<div>"+
+                                        "<p id='displayLitterm'>Cylinder @ Ksh.</p>"+
+                                        "<p id='displayLitter'>20</p>"+
+                                        "</div>"
+                                    );
+    });
+    $("#myButton").unbind("click").click(function()
+    {
+        var cash=$("#displayLitter").html();
+        var tot=$("#totalLiters").val();
+        var res=(cash+tot);
+        $("#show").append(
+                                        "<div>"+
+                                        "<p>"+res+"</p>"+
+                                        "</div>"
+                                    );
+    });
+  });
 });
 
 function myFunction()
